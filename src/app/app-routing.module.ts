d2e333aa9f3eaa9a07ee00c098bb68770335984b
Luis Rojas <lumrojasmu@unal.edu.co>
2023-06-05 15:05:31 -0500

    Subiendo ejercicio al github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'formularios',
+    loadChildren:() => import('./formularios/formularios.module').then(m => m.FormulariosModule)
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
